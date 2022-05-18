f701af9d6e55f7ee7e1b6dc32da983d291b476ac
boyhotkey96 <boyhotkey96@gmail.com>
2022-05-18 23:16:28 +0700

    w3_band: fix click submenu

--- w3_band/assets/js/menu.js
+++ w3_band/assets/js/menu.js
@@ -1,39 +1,38 @@
- // Menu
- var menuItems = document.querySelectorAll('.nav li a[href*="#"]')
- var menu = document.querySelector('.menu-bar')
- var header = document.querySelector('#header')
- var subMenu = document.querySelector('.nav .list-item .nav-sub')
- var sss = document.querySelector('.nav .list-item:hover .nav-sub')
- var headerHeight = header.clientHeight
+const menuItems = document.querySelectorAll('.nav li a[href*="#"]');
+const menu = document.querySelector(".menu-bar");
+const header = document.querySelector("#header");
+const subMenu = document.querySelector(".nav .list-item .nav-sub");
+const headerHeight = header.clientHeight;
+let count = 0;
 
- // Click icon menu
- menu.onclick = function () {
-     var isClosed = header.clientHeight
-     if (isClosed === headerHeight) {
-         header.style.height = 'auto'
-     } else {
-         header.style.height = '45px'
-     }
- }
 
- //Item Menu
- for (var i = 0; i < menuItems.length; i++) {
-     var count = 0;
+// handle click icon menu: open & close
+menu.onclick = () => {
+  const isClosed = header.clientHeight;
+  if (isClosed === headerHeight) {
+    header.style.height = "auto";
+  } else {
+    header.style.height = "45px";
+  }
+};
 
-     menuItems[i].onclick = function (event) {
-         var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('nav-sub')
+// handle click item menu
+for (const item of menuItems) {
+  item.onclick = () => {
+    const isParentMenu =
+      this.nextElementSibling &&
+      this.nextElementSibling.classList.contains("nav-sub");
+    isParentMenu ? null : header.style.height = null;
+  };
+}
 
-         if (isParentMenu) {
-             if (count == 0) {
-                 event.preventDefault();
-                 subMenu.style.display = 'block';
-                 count = 1;
-             } else {
-                 subMenu.style.display = 'none';
-                 count = 0;
-             }
-         } else {
-             header.style.height = null;
-         }
-     }
- }
+// handle click submenu: toggle
+subMenu.parentElement.addEventListener('click', () => {
+  if (count % 2 === 0) {
+    subMenu.style.display = 'block';
+    count++;
+  } else {
+    subMenu.style.display = 'none';
+    count++;
+  }
+})
